d16388942e66aea2e52f589f97dae832ce9c1c0e
Yonatan Hailu <jonathanhailu3@gmail.com>
2023-11-27 07:21:54 -0800

    done 5-9

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+w_f_3 = word[:3]
+w_l_2 = word[-2:]
+m_w = word[1:-1]
+print(f"First 3 letters: {w_f_3}")
+print(f"Last 2 letters: {w_l_2}")
+print(f"Middle word: {m_w}")
