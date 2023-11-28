0241205d8c3da9f0671425fb287944864d0ae5a3
Yonatan Hailu <jonathanhailu3@gmail.com>
2023-11-28 07:49:55 -0800

    ew

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print('89')
+# 6-print_comb3.py
+
+for anum1 in range(0, 10):
+    for anum2 in range(anum1 + 1, 10):
+        if anum1 == 8 and anum2 == 9:
+            print("{}{}".format(anum1, anum2))
         else:
-            print('{}{}, '.format(x, y), end='')
+            print("{}{}".format(anum1, anum2), end=", ")
